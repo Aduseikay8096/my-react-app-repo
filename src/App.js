c9739deda6f3b319289a12d30bebc7f70636e8cf
Aduseikay8096 <aduseikay8096@gmail.com>
2023-09-26 11:55:02 +0000

    Update logo and link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  // Define the new logo URL
+  const logoUrl = 'https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* Replace the logo with the new URL */}
+        <img src={logoUrl} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* Replace the link with the new URL */}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
